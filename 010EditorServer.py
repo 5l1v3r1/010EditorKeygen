c565ff71c07227c42b708e177cba28cbf7f5e9e6
{{2+2}} <52606428+0xd114@users.noreply.github.com>
2020-09-12 11:43:34 +0800

    Update 010EditorServer.py

--- 010EditorServer.py
+++ 010EditorServer.py
@@ -1,20 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from flask import Flask
+app = Flask(__name__)
 
-HOST = "127.0.0.1"
-PORT = 80
+@app.route('/cgibin/010editor_check_license_9b.php')
+def hello():
+    return "<ss>valid</ss>"
 
-class RequestHandler(BaseHTTPRequestHandler):
-	def do_GET(self):
-		self.send_response(200)
-		self.send_header("Content-Type", "text/html")
-		self.end_headers()
-		self.wfile.write("<ss>valid</ss>".encode("utf-8"))
- 
-def run_server():
-	server = HTTPServer((HOST, PORT), RequestHandler)
-	server.serve_forever()
- 
-if __name__ == "__main__":
-	run_server()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
